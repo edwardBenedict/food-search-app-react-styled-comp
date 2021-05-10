47765469daaabf3da71d0666ec946bd9d801c7c5
ed ben <edwardbenedict92@gmail.com>
2021-05-10 15:24:04 +0300

    feat: add bout page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,34 @@
 import React from "react";
+import {
+  AboutContainer,
+  HeaderContainer,
+  InfoContainer,
+  StyledImage,
+} from "./AboutStyle";
+import codingSvg from "../../assets/coding.svg";
 
 const About = () => {
   return (
-    <div>
-      <h2>About Me</h2>
-    </div>
+    <AboutContainer>
+      <StyledImage src={codingSvg} alt="coding" />
+      <HeaderContainer>
+        <h1>
+          About Software Developer <span>Ed Ben</span>
+        </h1>
+      </HeaderContainer>
+      <InfoContainer>
+        <h1>I'm Ed.</h1>
+        <h2>I’m currently learning Full-Stack Development Languages.</h2>
+        <h2>
+          I've already known JS, ReactJS, ReactNative, NodeJS, MongoDB,SQL,
+          Python, AWS Services.
+        </h2>
+        <h2>
+          <a href="mailto:edwardbenedict92@gmail.com">Send email</a> :
+          edwardbenedict92@gmail.com
+        </h2>
+      </InfoContainer>
+    </AboutContainer>
   );
 };
 
