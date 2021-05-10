99b9fd0c6359da5866829c879bbc8c80240d7c33
ed ben <edwardbenedict92@gmail.com>
2021-05-10 12:47:26 +0300

    feat: add privateRouter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Login from "../components/login/Login";
 import Main from "../components/recipe/Main";
 import Details from "../components/details/Details";
 import About from "../components/about/About";
+import PrivateRouter from "./PrivateRouter";
 
 function AppRouter() {
+  const [isAuth, setIsAuth] = useState(false);
+
+  const AuthContainer = () => (
+    <div>
+      <Navbar />
+      <PrivateRouter isAuth={isAuth} exact path="/" component={Main} />
+      <PrivateRouter isAuth={isAuth} path="/details" component={Details} />
+      <PrivateRouter isAuth={isAuth} path="/about" component={About} />
+    </div>
+  );
+
   return (
     <BrowserRouter>
-      <Navbar />
       <Switch>
-        <Route exact path="/" render={() => <Main />} />
-        <Route path="/login" component={Login} exact />
-        <Route path="/details" component={Details} />
-        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} exact setIsAuth={setIsAuth} />
+        <Route component={AuthContainer} />
       </Switch>
     </BrowserRouter>
   );
